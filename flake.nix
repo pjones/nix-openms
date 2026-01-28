4ca8d8a5316a8092e4eca16fbffd888244aaab40
Peter J. Jones <peter@jonesbunch.com>
2026-01-28 13:31:11 +0100

    openms: Prepare for OpenMS 3.6.0

--- flake.nix
+++ flake.nix
@@ -52,6 +52,10 @@
             boost = pkgs.boost186;
           };
 
+          py-build-cmake = pkgs.callPackage pkgs/py-build-cmake.nix {
+            python3Packages = self.packages.${system}.python3.pkgs;
+          };
+
           pyautowrap = pkgs.callPackage pkgs/pyautowrap.nix {
             python3Packages = self.packages.${system}.python3.pkgs;
           };
@@ -65,6 +69,7 @@
           python3 = pkgs.python3.override {
             packageOverrides = final: prev: {
               autowrap = self.packages.${system}.pyautowrap;
+              py-build-cmake = self.packages.${system}.py-build-cmake;
               pyopenms = self.packages.${system}.openms.pyopenms;
               pyopenms-viz = self.packages.${system}.pyopenms-viz;
             };
